e15ded2518fa9d3aa5e1189b22b4b57efd35bc38
ThanhNT <shintuken@mail.com>
2023-10-12 19:45:48 +0700

    init project 3D

---
+++ Assets/Script/Core/FollowCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+  public class FollowCamera : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+
+        // Update is called once per frame
+        private void LateUpdate()
+        {
+            transform.position = target.position;
+        }
+    }
+}
+
---
+++ Assets/Script/Movement/Mover.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.AI;
+using RPG.Core;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour, IAction
+    {
+        //[SerializeField] private Transform target;
+        private NavMeshAgent navMeshAgent;
+
+        private void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();   
+        }
+
+        private void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Cancel()
+        {
+            navMeshAgent.isStopped = true;
+        }
+
+        private void UpdateAnimator()
+        {
+            //Lấy velocity từ NavMesh Agentf
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            //Convert global sang localVelocity
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            //Lấy speed = localVelocity.Z
+            float speed = localVelocity.z;
+            //Set Float của animator
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+
+    }
+    
+    
+}
+
