e72f4a63f087c4f7c0c6c525a5a908235b0ff148
ThanhNT <shintuken@gmail.com>
2023-10-24 23:24:53 +0700

    Fixed Position when transition on new Scene

--- Assets/Script/SceneManagement/Portal.cs
+++ Assets/Script/SceneManagement/Portal.cs
@@ -4,58 +4,61 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
-public class Portal : MonoBehaviour
+namespace RPG.Core
 {
-    [SerializeField] private int sceneToLoad = -1;
-    [SerializeField] private Transform spawnPoint;
-    private GameObject player;
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] private int sceneToLoad = -1;
+        [SerializeField] private Transform spawnPoint;
 
-    private void Start()
-    {
-        
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Player")
+        private void OnTriggerEnter(Collider other)
         {
-            StartCoroutine(LoadScene());
+            if (other.tag == "Player")
+            {
+                StartCoroutine(LoadScene());
+            }
+
+
+            IEnumerator LoadScene()
+            {
+                //DontDestroyOnLoad dosen't work when the GameObject is child of something, so must set Parent to NULL
+                //transform.parent = null;
+
+                DontDestroyOnLoad(gameObject);
+                yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+                Portal otherPortal = GetAnotherPortal();
+                UpdatePlayer(otherPortal);
+
+                Destroy(gameObject);
+            }
         }
 
-
-        IEnumerator LoadScene()
+        private void UpdatePlayer(Portal otherPortal)
         {
-            AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad);
-            DontDestroyOnLoad(this.gameObject);
-            yield return asyncOperation;
-
-            Portal otherPortal = GetAnotherPortal();
-            UpdatePlayer(otherPortal);
-                            
-            Destroy(this.gameObject);
-        }
-    }
-
-    private void UpdatePlayer(Portal otherPortal)
-    {
-        player = GameObject.FindWithTag("Player");
-        if (otherPortal != null)
-        { 
+            
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().enabled = false;
             player.transform.position = otherPortal.spawnPoint.position;
             player.transform.rotation = otherPortal.spawnPoint.rotation;
+            player.GetComponent<NavMeshAgent>().enabled = true;
 
         }
-    }
 
-    private Portal GetAnotherPortal()
-    {
-        foreach (Portal portal in FindObjectsOfType<Portal>())
+        private Portal GetAnotherPortal()
         {
-            if (portal == this) continue;
-            return portal;
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+
+                return portal;
+            }
+            return null;
+
         }
-        return null;
-       
     }
 }
+
