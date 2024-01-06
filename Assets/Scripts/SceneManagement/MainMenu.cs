a380729fef3436c4df0bb118f44ac1473c89b924
shintuken <shintuken@gmail.com>
2024-01-07 00:13:24 +0700

    Add Main Menu Script
    Add Main Menu Script

---
+++ Assets/Scripts/SceneManagement/MainMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        int currentScene = SceneManager.GetActiveScene().rootCount;
+        Scene nextScene = SceneManager.Se
+        SceneManager.SetActiveScene();
+    }
+}
